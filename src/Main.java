3262b7af64d3c810a4ee23b74e99c502d1d9566f
Muhammed Bilal M N <muhammedbilalmn585@gmail.com>
2025-09-06 15:30:17 +0530

    added some other files

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.time.LocalDate;
+
+public class  Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//        Premitive datatypes
+        int num = 100;
+        byte numbyte = -128;
+        short numshort = 3_598;
+        float number = 3.14f;
+        double numdouble = 3.144333;
+        char str = 'a';
+        long lg = 12_34_5509l;
+        boolean bl = false;
+        System.out.println(num);
+        System.out.println(numbyte);
+        System.out.println(numshort);
+        System.out.println(number);
+        System.out.println(numdouble);
+        System.out.println(str);
+        System.out.println(bl);
+
+//        Reference datatypes or non-primitive datatypes
+
+        String strg = new String("Bilal");
+        System.out.println(strg);
+        System.out.println(strg.toUpperCase());
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
     }
 }
