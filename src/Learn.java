bf7b5b559e3dc6faf6011b39d33f926b37668f5c
Muhammed Bilal M N <muhammedbilalmn585@gmail.com>
2025-09-09 22:47:06 +0530

    updating file

--- src/Learn.java
+++ src/Learn.java
@@ -1,5 +1,24 @@
+import java.util.Date;
+//package com.bilal;
 public class Learn {
     public static void main(String[] args){
+
+//        character
+        char a = 'a';
+
+//        string
+        String name = "Muhammed Bilal";
+        System.out.println(name);
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(1));
+        System.out.println(name.contains("code"));
+        System.out.println(name.equals("Bilal"));
+
+//        the import statement is use for importing specific packages.
+//        above we have specified the import Date from the java.util package.
+
+
 
     }
 }
